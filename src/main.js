4d9dd284c78fe50a547dab2aa53e3c0d0edc5b9f
Drew Peterson <drew.a.peterson@hotmail.com>
2021-05-12 16:44:34 -0700

    Separate logic for front and back end

--- src/main.js
+++ src/main.js
@@ -2,3 +2,25 @@ import './css/styles.css';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import { stateControl, changeState } from './plant.js';
+
+
+const feed = changeState("soil")(1);
+const blueFood = changeState("soil")(5);
+const greenFood = changeState("soil")(10);
+const hydrate = changeState("water")(1);
+const superWater = changeState("water")(5);
+const sun = changeState("light")(1);
+const directSun = changeState("light")(5);
+
+
+$(document).ready(function() {
+  $('#feed').click(function() {
+    const newState = stateControl(blueFood);
+    $('#soil-value').text(`Soil: ${newState.soil}`);
+  });
+  $('#show-state').click(function() {
+    const currentState = stateControl();
+    $('#soil-value').text(`Soil: ${currentState.soil}`);
+  });
+});
---
+++ src/plant.js
@@ -0,0 +1,23 @@
+const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  };
+};
+
+const stateControl = storeState();
+
+const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop]: (state[prop] || 0 ) + value
+    });
+  };
+};
+
+
+export { stateControl, changeState };
+
